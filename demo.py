8994b5edf39ef1050016ea86fdbf286285eba041
phone888 <linth051@gmail.com>
2024-09-06 10:22:41 +0700

    update demo.py

---
+++ demo.py
@@ -0,0 +1,4 @@
+if 5 > 2:
+    print("You awe some")
+    if 5 < 2:
+        print("You are stupid")
---
+++ index.ts
@@ -0,0 +1,6 @@
+// this is python
+const Universe = (name: string, address: number) => {
+  return `Name: ${name} Number: ${address}`;
+};
+const res = Universe("world", 123);
+console.log(res);
